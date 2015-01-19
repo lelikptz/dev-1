dffdad9d93ddd9f139a7755a0077f55cd6008a91
Aleksey Orlov <orlov@farbik.ru>
2015-01-19 23:19:07 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    var input = $('#sign-email');
+    input.on('focus', function () {
+        $('#sign-email').prev('label').addClass('active');
+    });
+
+    input.on('blur', function () {
+        if (input.val().length == 0) {
+            input.prev('label').removeClass('active');
+        }
+    });
+
+    if (input.val() != '') {
+        input.prev('label').addClass('active');
+    }
+
+    $(document).on('click', 'a', function () {
+        return false;
+    });
+});
