460fa3ca34384536c004a00481c510a5c4681d2f
Aleksey Orlov <lelikptz@icloud.com>
2015-09-01 03:14:48 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 $(document).ready(function () {
 
     var input = $('#sign-email');
-
-    input.on('focus', function () {
-        input.prev('label').addClass('active');
-    });
-
-    input.on('blur', function () {
-        if (input.val().length == 0) {
-            input.prev('label').removeClass('active');
-        }
-    });
-
+    input
+        .on('focus', function () {
+            input.prev('label').addClass('active');
+        })
+        .on('blur', function () {
+            if (input.val().length == 0) {
+                input.prev('label').removeClass('active');
+            }
+        });
     if (input.val() != '') {
         input.prev('label').addClass('active');
     }
+
+    $('.calendar_t').on('click', 'td.day, td.weekend', function () {
+       $(this).toggleClass('active');
+    });
 
     $(document).on('click', 'a', function () {
         return false;
